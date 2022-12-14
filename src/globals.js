3b696376980d61cda11d33cce2b9dea58cb86787
Céo <noah.kd@gmail.com>
2022-12-14 17:18:13 -0300

    UOFUGFVOL
     Changes to be committed:
    	renamed:    firebase/login.html -> firebase/login-email.html
    	modified:   src/globals.js

--- src/globals.js
+++ src/globals.js
@@ -4,6 +4,6 @@ window.addEventListener( "load", ev => {
 	var links = document.querySelectorAll( "li[link]" );
 
 	links.forEach( link => link.addEventListener( "click", ev => {
-		window.open( `https://ceo-sammarco.github.io/sketchware/${link.getAttribute( "link" )}`, "_self" );
+		window.open( link.getAttribute( "link" ), "_self" );
 	} ) );
 } );
